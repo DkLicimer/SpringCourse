65399a3c2295733985e899cf568f1ce941c97775
Dmitry Kurbangaleev <bachyachve@gmail.com>
2024-06-13 00:58:09 +0900

    taskforwork

--- src/main/java/ru/kurbangaleev/SpringCourse/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.kurbangaleev.SpringCourse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.kurbangaleev.SpringCourse.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
