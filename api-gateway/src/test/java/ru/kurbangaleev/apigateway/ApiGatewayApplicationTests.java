6faea6c6476109d04c7cf61132019eac15d560ee
Dmitry Kurbangaleev <bachyachve@gmail.com>
2024-08-13 18:11:59 +0900

    api gateway

--- api-gateway/src/test/java/ru/kurbangaleev/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/ru/kurbangaleev/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.kurbangaleev.eurekaserver;
+package ru.kurbangaleev.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
