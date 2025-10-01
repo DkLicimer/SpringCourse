68bc184f6f9f0dffb370817e2b5ff51be27408fd
Dmitry Kurbangaleev <bachyachve@gmail.com>
2025-10-01 15:32:19 +0900

    pragmat-website

--- ZabGU_SPACE/src/main/java/ru/kurbangaleev/zabgu_space/service/EmailService.java
+++
@@ -1,5 +0,0 @@
-package ru.kurbangaleev.zabgu_space.service;
-
-public interface EmailService {
-    void sendSimpleMessage(String to, String subject, String text);
-}
--- ZabGU_SPACE/src/main/java/ru/kurbangaleev/zabgu_space/service/RoomService.java
+++
@@ -1,8 +0,0 @@
-package ru.kurbangaleev.zabgu_space.service;
-
-import ru.kurbangaleev.zabgu_space.entity.Room;
-import java.util.List;
-
-public interface RoomService {
-    List<Room> getAllRooms();
-}
