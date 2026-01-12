50fedf6fc6deb152bbeff88a3f6aa19572566103
Dmitry Kurbangaleev <bachyachve@gmail.com>
2026-01-12 09:34:35 +0900

    exam

--- inetprogashop/shop/src/main/java/ru/kurbangaleev/shop/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.kurbangaleev.shop.repositories;
-
-import ru.kurbangaleev.shop.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
