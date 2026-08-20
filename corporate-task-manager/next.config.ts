89caad5b10f5dfc781d43264b6c9176bb2bb9531
Alandale <bachyachve@gmail.com>
2026-08-20 22:38:58 +0900

    20082026clear

--- corporate-task-manager/next.config.ts
+++
@@ -1,9 +0,0 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Исключаем Prisma и pg из компиляции Turbopack
-  serverExternalPackages: ["@prisma/client", "pg"],
-};
-
-export default nextConfig;
